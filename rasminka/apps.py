ae2deef018798fbc0f87c7b59d09b7241a06a8d9
Tigls <dimakhort93@gmail,com>
2017-12-15 01:27:34 +0200

    First commit

---
+++ rasminka/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RasminkaConfig(AppConfig):
+    name = 'rasminka'
---
+++ rasminka/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+render
+# Create your views here.
