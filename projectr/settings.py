72fa40c61b4ef55a76a5e8142752011fa975d606
Tigls <khort@rasminka.ch>
2017-12-15 18:30:25 +0200

    - added template dir and corrected views

--- projectr/settings.py
+++ projectr/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'projectr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates/rasminka')
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- rasminka/views.py
+++ rasminka/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def show_page(request, page_url=None):
     page = get_object_or_404(Page, url=page_url)
-    render(request, 'page.html')
+    render(request, 'page_content.html')
