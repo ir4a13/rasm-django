205a80826169c9a2f50acfa8a2c36438cfcc92b1
Tigls <khort@rasminka.ch>
2017-12-15 18:34:11 +0200

    - added template dir and corrected views

--- rasminka/urls.py
+++ rasminka/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.rasminka, name='rasminka'),
+    path('/', 'views.show_page'),
     path('/page_url', 'views.show_page'),
 
 ]
