88a80f25faab19ef0c1e7b4df9a69553857da6b3
Tigls <khort@rasminka.ch>
2017-12-15 18:42:02 +0200

    corrections

--- rasminka/urls.py
+++ rasminka/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.show_page),
-    path('/page_url', views.show_page),
+    path('', views.show_page),
+    path('page_url', views.show_page),
 
 ]
--- rasminka/views.py
+++ rasminka/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_page(request, page_url=None):
-    page = get_object_or_404(Page, url=page_url)
-    render(request, 'page_content.html')
+    #page = get_object_or_404(Page, url=page_url)
+    return render (request, 'page_content.html')
